c3e772638cc05641f94224a21b7adfc5467d19c2
Abhishek Verma <69351665+iam-abhi@users.noreply.github.com>
2022-10-03 16:49:05 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -7,41 +7,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCases {
 
-    Vehicle vehicle;
-
-    // @Test
-    // public void go_increasesOdometerBy100AndDecreasesGallonsOfGasBy5WhenMilesPerGallonIs20() {
-    //     givenVehicleHas0Odometer15GallonsOfGasAnd20MilesPerGallon();
-    //     whenDrive100();
-    //     thenOdometerIncreasesBy100AndGallonsOfGasDecreasesBy5();
-    // }
-
-    // @Test
-    // public void go_increasesOdometerBy100AndDecreasesGallonsOfGasBy5WhenMilesPerGallonIs20_2() {
-    //     givenVehicleHas0Odometer15GallonsOfGasAnd20MilesPerGallon();
-    //     whenDrive100();
-    //     thenOdometerIncreasesBy100AndGallonsOfGasDecreasesBy5_2();
-    // }
-
-    // private void givenVehicleHas0Odometer15GallonsOfGasAnd20MilesPerGallon() {
-    //     vehicle = new Vehicle();
-    //     vehicle.setGallonsOfGas(15);
-    //     vehicle.setOdometer(0);
-    //     vehicle.setMilesPerGallon(20);
-    // }
-
-    // private void whenDrive100() {
-    //     vehicle.go(100);
-    // }
-
-    // private void thenOdometerIncreasesBy100AndGallonsOfGasDecreasesBy5() {
-    //     assertEquals(10, vehicle.getGallonsOfGas());
-    //     assertEquals(105, vehicle.getOdometer());
-    // }
-
-    // private void thenOdometerIncreasesBy100AndGallonsOfGasDecreasesBy5_2() {
-    //     assertEquals(10, vehicle.getGallonsOfGas());
-    //     assertEquals(100, vehicle.getOdometer());
-    // }
-
 }
